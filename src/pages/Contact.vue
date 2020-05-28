2148967c7876be9288874a49f6a5b4e0e4ae0b21
Tamas Erdelyi <terdelyi@gmail.com>
2020-05-29 00:03:25 +0100

    Update form success page

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -6,12 +6,11 @@
     <div class="font-display mb-4">Recruiters you shall not pass here! But if you're not one of them and you have an exciting project idea fill me in with the details in case I might be interested.</div>
 
     <div class="mb-10">
-      <form name="contact" method="POST" data-netlify="true">
-        <input type="hidden" name="action" value="/contact/thanks">
+      <form name="contact" action="/contact/thanks" method="POST" data-netlify="true">
         <div class="my-3"><input type="text" name="name" class="text-input" placeholder="Name"></div>
-        <div class="my-3"><input type="email" name="name" class="text-input" placeholder="Email"></div>
+        <div class="my-3"><input type="email" name="email" class="text-input" placeholder="Email"></div>
         <div class="my-3"><textarea name="message" class="text-input h-32" placeholder="Message"></textarea></div>
-        <div><button type="submit" class="btn">Submit</button></div>
+        <div><button type="submit" class="btn">Send</button></div>
       </form>
     </div>
 
