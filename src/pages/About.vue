a76435d0a5ce3a1d215a7a6ad3c9ddc0f44438ae
Tamas Erdelyi <tamas@linesgroup.co.uk>
2020-06-02 21:56:43 +0100

    Update the about me page

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -4,27 +4,12 @@
     <h1 class="page-title">I hope you're sitting comfortable.</h1>
 
     <div class="has-content mb-8">
-      <p>I first met with a real computer when I was around ten. I think it was one of our neighbour's Commodore 64.
-        Therefore my dad had to buy a Commodore for us. Then my neighbour upgraded to a Pentium PC. We got that too, obviously.
-        Jumping a few years forward my parents enrolled me in course which was all about Windows 3.1 and coding in Basic.
-        I finished it before I turned to fourteen. The invigilator barely gave me the certificate because I wasn't "old enough"
-        yet to have a personal ID. I was already in a big love with movies, then computers seemed to be my new passion.</p>
-      <p>I used my PC to write songs and movie reviews, but it also helped me to manage my massive VHS catalogue when
-        I was running a totally illegal renting business as a kid. I copied and ripped apart the codebase of various disk
-        magazines to see how they worked. At one point I built an awesome PowerPoint presentation, compiled to an app and
-        sold it to another kid with less knowledge about this. On the way I was learning Turbo Pascal, Perl, how drew with
-        Corel products. Then the internet boom came. I started to build websites for Netscape Navigator through a dial-up
-        modem with FrontPage Express and Paint Shop Pro, using the first versions of HTML and CSS. Then in my twenties I
-        founded a movie site and began to ship real projects for real companies.</p>
-      <p>2009 came, I desperately needed something permanent. Years later I was fed up with agency projects, so I quit to
-        be a freelancer and work for my own clients. After two struggling years bills finally won, independence lost (for now!) and I've been an employee since 2013.
-        In the last few years I worked from small agencies to even big, corporate machines.</p>
-      <p>I've been building websites for 18 years now. I started as a web designer, learned site-building to be more effective,
-        but finally trained myself into a reckless PHP developer. And when I thought backend is perfectly enough me,
-        I began to interest the frontend again, jamstack, mobile development, servers, the whole branch.</p>
-      <p>Today I think myself as an open-minded software engineer, eager to learn and try everything which moves himself
-        forward and get his brain busy.</p>
-      <p>In my free time I'm just a geek dad in his late 30s.</p>
+      <p>I first met with a real computer when I was around ten. I think it was one of our neighbour's Commodore 64. Therefore my dad had to buy a Commodore for us. Then my neighbour upgraded to a Pentium PC. Surprise, we got that, too. Jumping a few years ahead, my parents enrolled me in a course about Windows 3.1 and coding in Basic. I finished it before I turned to 14. The invigilator barely gave me the certificate because I wasn't "old enough" yet to have a personal ID. I was already in big love with movies, and computers seemed to be my new passion.</p>
+      <p>I used my PC to write songs and movie reviews, but it also helped me to manage my massive VHS catalogue when I was running a completely illegal renting business as a kid. I copied and ripped apart the codebase of various disk magazines to see how they worked. At one point, I built a kick-ass PowerPoint presentation, compiled to an app and sold it to another kid with less knowledge about this. On the way I was learning Turbo Pascal, Perl, how drew with Corel products. Then the internet boom came. I started to build websites for Netscape Navigator through a dial-up modem with FrontPage Express and Paint Shop Pro, using the first versions of HTML and CSS. Then in my twenties, I founded a movie site and began to ship real projects for real companies.</p>
+      <p>2009 came, I desperately needed something permanent. Years later, I was fed up with agency projects, so I quit to be a freelancer and work for my clients. After two struggling years bills finally won, independence lost (for now!) and I've been an employee since 2013. In the last few years, I worked from small agencies to even big, corporate machines.</p>
+      <p>I've been building websites for 18 years now. I started as a web designer. Later learned site-building to be more productive, but finally trained myself into a reckless PHP developer. And when I thought backend is perfectly enough me, I began to interest the frontend again, jamstack, mobile development, servers, you know the whole set.</p>
+      <p>Today I think myself as an open-minded software engineer, who's eager to learn and try everything which moves him forward and get his brain busy.</p>
+      <p>But in my free time, I'm just a geek dad in his late 30s.</p>
     </div>
 
   </Layout>
