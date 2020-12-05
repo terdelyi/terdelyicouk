be9e5e92e1db4a27d2a9effc59f1e154739709e4
Tamas Erdelyi <terdelyi@gmail.com>
2020-12-05 01:09:31 +0000

    Remove accidental characters from index, adds Music block to Uses

--- src/pages/Uses.vue
+++ src/pages/Uses.vue
@@ -8,21 +8,21 @@
       <h2>Hardware</h2>
       <ul>
         <li>
-          <h4><a href="https://amzn.to/3lF833c">MacBook Pro 13-inch, 2017</a> (2.3 GHz, 16 GB, 512 SSD) for personal use</h4>
+          <h4><a href="https://amzn.to/3lF833c">MacBook Pro 13-inch, 2017  (2.3 GHz, 16 GB, 512 SSD)</a></h4>
           <div>Let's be clear: I hate it. It's one of the worst laptops made by Apple. Warmer than a hot plate, the battery drained after 2
           years and the keyboard sucks, several keys are just not working anymore. And you can't say I'm a heavy user. When I bought this was the only affordable Mac on the market and I needed to
             carry on with side-projects.</div>
         </li>
         <li>
-          <h4><a href="https://www.apple.com/uk/shop/buy-mac/macbook-pro/13-inch">MacBook Pro 13-inch, 2020</a> (2.7 GHz, 32 GB, 256 SSD) for work</h4>
-          <div>The Touch Bar is still an unnecessary gimmick, but the new-old keyboard is finally back! The performance is more than enough what I'm using for and it gave back my faith to Apple computers. I can't wait to see what's M1 is capable for!</div>
+          <h4><a href="https://www.apple.com/uk/shop/buy-mac/macbook-pro/13-inch">MacBook Pro 13-inch, 2020 (2.7 GHz, 32 GB, 256 SSD)</a></h4>
+          <div>I've got it from my workplace. The Touch Bar is still an unnecessary gimmick, but the new-old keyboard is finally back! The performance is more than enough what I'm using for and it gave back my faith to Apple computers. I can't wait to see what's M1 is capable for!</div>
         </li>
         <li>
-          <h4><a href="https://amzn.to/3opDjVo">Dell U2715H</a> monitor</h4>
+          <h4><a href="https://amzn.to/3opDjVo">Dell U2715H</a></h4>
           <div>I don't believe in multiple monitor setups and multitasking, the bigger the best. Sometimes I'm dreaming of a 34" 4K monitor, but until she dies it's gonna stay on my desk.</div>
         </li>
         <li>
-          <h4><a href="https://amzn.to/2VzR0ot">Apple Magic Keyboard</a> with numeric keys, Space Gray</h4>
+          <h4><a href="https://amzn.to/2VzR0ot">Apple Magic Keyboard, Space Gray</a></h4>
           <div>I'm keen to jump on the mechanic keyboard train, but before I couldn't resist this beauty. Scissors mechanism as it best. I'm only sad because the lack of the background lights.</div>
         </li>
         <li>
@@ -30,27 +30,58 @@
           <div>I only looked away once to the Magic Mouse and tried an MX Master 2, but I couldn't go back anymore. It's more ergonomic and I honestly love the endless scrolling button.</div>
         </li>
         <li>
-          <h4><a href="https://amzn.to/36BuUYY">Logitech c920 HD Pro</a> webcam</h4>
+          <h4><a href="https://amzn.to/36BuUYY">Logitech c920 HD Pro</a></h4>
           <div>Great for HD conferences, especially in these remote working times. Took me a while to buy it, but I use it everyday since.</div>
         </li>
         <li>
-          <h4><a href="https://amzn.to/3mG77N9">Blue Yeti</a> microphone</h4>
+          <h4><a href="https://amzn.to/3mG77N9">Blue Yeti</a></h4>
           <div>It's a great microphone, but not for podcasting as it records every tiny details in the background. However I've stopped podcasting recently so replacing it is not my priority at the moment.</div>
         </li>
       </ul>
       <h2>Development</h2>
       <ul>
         <li>
-          <h4><a href="https://www.jetbrains.com/phpstorm/">PHPStorm</a> by JetBrains</h4>
-          <div>I had to give it multiple tries, but we're officially dating in the last two years. When I need something quicker I just launch Sublime Text 3 instead.</div>
+          <h4><a href="https://www.jetbrains.com/phpstorm/">PHPStorm</a></h4>
+          <div>I had to give this IDE multiple tries, but we've been officially dating in the last two years. It feels an overkill, because it has built-in EVERYTHING, but once you start using it you cannot go back to blindness. And light theme, obviously.</div>
         </li>
         <li>
-          <h4><a href="https://www.araelium.com/querious">Querious 3</a> by Araelium</h4>
+          <h4><a href="https://www.jetbrains.com/phpstorm/">Sublime Text</a></h4>
+          <div>When I need something to edit quickly or there's a large database dump or CSV I just launch Sublime instead.</div>
+        </li>
+        <li>
+          <h4><a href="https://www.araelium.com/querious">Querious 3</a></h4>
           <div>I was a Sequel Pro user for a long time, but after last year it left without a maintainer I thought it's time to pay for something permanent. I've tried multiple tools like MySQL Workbench and TablePlus, but I picked Querious at the end.</div>
         </li>
         <li>
           <h4><a href="https://iterm2.com/">iTerm 2</a> with <a href="https://github.com/ohmyzsh/ohmyzsh">OhMyZsh</a></h4>
           <div>I always used the official Terminal app on macOS then I gave a go for iTerm 2 and I liked it. Zsh bash is also built-in since Catalina, but OhMyZsh gives it more spice.</div>
+        </li>
+        <li>
+          <h4><a href="https://laravel.com/docs/valet">Laravel Valet</a></h4>
+          <div>I was a massive Homestead user for years on Mac after I ditched MAMP Pro then realised most of my projects don't require any virtualization or specific packages. It's still my main environment, but the more I learn about Docker it's possible that one day it follows MAMP's fate.</div>
+        </li>
+      </ul>
+      <h2>Music</h2>
+      <ul>
+        <li>
+          <h4><a href="https://amzn.to/3lJxwbR">KRK Rokit 5 G4 White Noise</a></h4>
+          <div>I had a pair of Mackie MR5 MK3 for years, but after I moved to the UK I had to sell it. I was sharing a Sonos One with my son for music listening, but this year I decided it's time to get back to music producing and I want real stereo speakers on my desk. They are amazing!</div>
+        </li>
+        <li>
+          <h4><a href="https://www.ableton.com/en/live/">Ableton Live</a></h4>
+          <div>I was on Fast Tracker 2 in my childhood and a Live user in my adulthood. I've tried Logic and Fruity Loops, but nothing beats the layout and the possibilities with Live.</div>
+        </li>
+        <li>
+          <h4><a href="https://amzn.to/2JPHNG8">Akai APC40 mkII</a></h4>
+          <div>Great for writing songs from loops, live acts and DJing. It's a perfect match for Ableton Live.</div>
+        </li>
+        <li>
+          <h4><a href="https://amzn.to/3gdMUfk">Akai MPK Mini mkII</a></h4>
+          <div>It's an awesome little tool for writing music and playing with VSTi-s. Sometimes I wish I would go for a bigger one with 49 keys, but hey, the fact that is fit in your backpack is unbeatable.</div>
+        </li>
+        <li>
+          <h4><a href="https://amzn.to/2VEkfGN">EMU Xboard 25</a></h4>
+          <div>It is my first MIDI keyboard. About 12 years old, I wanted to sell it multiple times, but somehow stayed with me. The knobs and triggers are old-school comparing to the Akai Mini, but I guess that also gives its charm.</div>
         </li>
       </ul>
     </div>
