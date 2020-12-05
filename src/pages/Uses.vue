7487f12e9bc6e6b39f7a78a415be54d541a9e9ea
Tamas Erdelyi <terdelyi@gmail.com>
2020-12-05 01:25:50 +0000

    Additional updates on Uses

--- src/pages/Uses.vue
+++ src/pages/Uses.vue
@@ -8,21 +8,21 @@
       <h2>Hardware</h2>
       <ul>
         <li>
-          <h4><a href="https://amzn.to/3lF833c">MacBook Pro 13-inch, 2017  (2.3 GHz, 16 GB, 512 SSD)</a></h4>
+          <h4><a href="https://amzn.to/3lF833c">MacBook Pro (13-inch, 2017, 2.3 GHz Dual-Core Intel Core i5, 16 GB, 512 GB SSD)</a></h4>
           <div>Let's be clear: I hate it. It's one of the worst laptops made by Apple. Warmer than a hot plate, the battery drained after 2
           years and the keyboard sucks, several keys are just not working anymore. And you can't say I'm a heavy user. When I bought this was the only affordable Mac on the market and I needed to
             carry on with side-projects.</div>
         </li>
         <li>
-          <h4><a href="https://www.apple.com/uk/shop/buy-mac/macbook-pro/13-inch">MacBook Pro 13-inch, 2020 (2.7 GHz, 32 GB, 256 SSD)</a></h4>
-          <div>I've got it from my workplace. The Touch Bar is still an unnecessary gimmick, but the new-old keyboard is finally back! The performance is more than enough what I'm using for and it gave back my faith to Apple computers. I can't wait to see what's M1 is capable for!</div>
+          <h4><a href="https://www.apple.com/uk/shop/buy-mac/macbook-pro/13-inch">MacBook Pro (13-inch, 2020, 2.3 GHz Quad-Core Intel i7, 32 GB, 500 GB SSD)</a></h4>
+          <div>I got it from my workplace. The Touch Bar is an unnecessary gimmick, but the new-old keyboard is finally back! The performance is more than enough what I'm using for and it gave back my faith to Apple computers. Batteries could be better, I can't wait to see what's M1 is capable for!</div>
         </li>
         <li>
           <h4><a href="https://amzn.to/3opDjVo">Dell U2715H</a></h4>
           <div>I don't believe in multiple monitor setups and multitasking, the bigger the best. Sometimes I'm dreaming of a 34" 4K monitor, but until she dies it's gonna stay on my desk.</div>
         </li>
         <li>
-          <h4><a href="https://amzn.to/2VzR0ot">Apple Magic Keyboard, Space Gray</a></h4>
+          <h4><a href="https://amzn.to/2VzR0ot">Magic Keyboard with Numeric Keypad - Space Gray</a></h4>
           <div>I'm keen to jump on the mechanic keyboard train, but before I couldn't resist this beauty. Scissors mechanism as it best. I'm only sad because the lack of the background lights.</div>
         </li>
         <li>
@@ -36,6 +36,14 @@
         <li>
           <h4><a href="https://amzn.to/3mG77N9">Blue Yeti</a></h4>
           <div>It's a great microphone, but not for podcasting as it records every tiny details in the background. However I've stopped podcasting recently so replacing it is not my priority at the moment.</div>
+        </li>
+        <li>
+          <h4><a href="https://amzn.to/3gfP7ae">Sony WH‑1000XM3</a></h4>
+          <div>I'm not a big fan of Sony products, but this one really kicks Bose Quietcomfort 35 in a butt. I wish it could connect to multiple devices at the same time (they fixed this in MK4) and it could turn off automatically when I'm using it with the Macbook.</div>
+        </li>
+        <li>
+          <h4><a href="https://www.apple.com/uk/airpods-pro/">AirPods Pro</a></h4>
+          <div>I know, what the hell do I need another headset for?! Try it for phone calls, listening podcasts and music in the bed and you'll understand. Brilliant earphones with a very cool charging method.</div>
         </li>
       </ul>
       <h2>Development</h2>
@@ -82,6 +90,10 @@
         <li>
           <h4><a href="https://amzn.to/2VEkfGN">EMU Xboard 25</a></h4>
           <div>It is my first MIDI keyboard. About 12 years old, I wanted to sell it multiple times, but somehow stayed with me. The knobs and triggers are old-school comparing to the Akai Mini, but I guess that also gives its charm.</div>
+        </li>
+        <li>
+          <h4><a href="https://amzn.to/2VHaFmp">Audio-Technica ATH-M50X</a></h4>
+          <div>Wireless isn't good enough for producing, you need cables and instant feedback. Be careful with the volume, it's really crisp and loud!</div>
         </li>
       </ul>
     </div>
