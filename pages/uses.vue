23f2252a730dd69c3bc9a0865457dd3de3d54a23
Tamas Erdelyi <terdelyi@users.noreply.github.com>
2023-06-10 22:51:37 +0100

    Fix contact form (#10)

--- pages/uses.vue
+++ pages/uses.vue
@@ -7,41 +7,45 @@
         <div class="md:w-1/2 md:pr-6">
           <div>
             <h4><a href="https://amzn.to/3lF833c">MacBook Air (13-inch, 2021, 2.3 GHz Dual-Core Intel Core i5, 16 GB, 512 GB SSD)</a></h4>
-            <p>Switched from 13" MacBook Pro 2017 which was the worst machine what I've ever had from Apple. The M1 Air is exactly the opposite: fast, lightweight and perfect for a home computer.</p>
+            <p>Switched from a 13" MacBook Pro 2017 which was clearly the worst machine I've ever bought from Apple.
+              The M1 Air is absolutely the opposite: fast, lightweight and perfect for an everyday home computer.</p>
           </div>
           <div>
             <h4><a href="https://www.apple.com/uk/shop/buy-mac/macbook-pro/13-inch">Work: MacBook Pro (15-inch, 2020, 2.3 GHz Quad-Core Intel i7, 32 GB, 500 GB SSD)</a></h4>
-            <p>It's one of the last dinosaurs, but good enough for coding and Docker.</p>
+            <p>It's one of the last dinosaurs, but good enough for coding and it doesn't smoke when
+              I'm using Docker or Chrome.</p>
           </div>
           <div>
             <h4><a href="https://amzn.to/3opDjVo">Dell U2715H</a></h4>
-            <p>I don't believe in multi-monitor setup and multitasking, the bigger the best with less distractions.</p>
+            <p>The bigger the best with less distractions, I don't believe in multi-monitor setups or multitasking.</p>
           </div>
           <div>
             <h4><a href="https://amzn.to/2VzR0ot">Keychron K1 Bluetooth Mechanical Keyboard</a></h4>
-            <p>I jumped on the mechanic keyboard train after using Apple keyboards in the last decade. It has shiny RGB lights which looks cool, but I barely turn them on and sadly the battery seems to be dying in less than a year.</p>
+            <p>I jumped on the mechanic keyboard train after using Apple keyboards for a decade. It has shiny RGB
+              lights which looks cool, but I barely turn them on and sadly the batteries aren't the best in
+              the category.</p>
           </div>
         </div>
         <div class="md:w-1/2 md:pl-6">
           <div>
             <h4><a href="https://amzn.to/3mFvkTX">Logitech MX Master 3</a></h4>
-            <p>I only looked away once to the Magic Mouse and tried an MX Master 2, and I love it! It's more ergonomic
-              and I the endless scrolling is a game changer.
+            <p>I only looked away once from the Magic Mouse to try an MX Master, and absolutely loved it! It's more ergonomic
+              and I just love the endless scrolling wheel.
             </p>
           </div>
           <div>
             <h4><a href="https://amzn.to/36BuUYY">Logitech c920 HD Pro</a></h4>
-            <p>Great for HD conferences, less ideal for YouTube content creation.</p>
+            <p>Great for HD conferences, but less ideal for YouTube content creation.</p>
           </div>
           <div>
             <h4><a href="https://www.apple.com/uk/airpods-pro/">AirPods Pro</a></h4>
-            <p>The first AirPods which fits in my ear. Very comfy and charging fast. I also have a Sony WH‑1000XM3 when
-              I need some punchy bass.
+            <p>The first AirPods which fits in my ear. Very comfy, sounds nice and it's charging fast.
+              I also have a Sony WH‑1000XM3, just in case I need some punchy bass.
             </p>
           </div>
           <div>
             <h4><a href="https://www.apple.com/uk/airpods-pro/">Apple iPhone 12 - (PRODUCT)Red</a></h4>
-            <p>Loyal iPhone user since the 3GS.</p>
+            <p>I'm a loyal iPhone user since the 3GS.</p>
           </div>
         </div>
       </div>
@@ -50,37 +54,32 @@
           <h2>On my desk</h2>
           <div>
             <h4><a href="https://bit.ly/2X3C0zX">Autonomous SmartDesk 2 DIY standing frame</a></h4>
-            <p>I'm not an avid user of standing desks, but it's good to have an option to change positions. I have three
-              height set: sitting, standing, and "making music" which is half between them.
+            <p>I'm not an avid user of standing desks, but it's good to have an option to switch postures. I have three
+              heights set: sitting, standing, and "making music" which is somewhere in the middle. I skipped the top,
+              because that's overpriced.
             </p>
           </div>
           <div>
-            <h4><a href="https://www.ikea.com/gb/en/p/oevraryd-table-top-bamboo-90305723/">IKEA Övraryd table top,
-              bamboo,
-              150x78</a></h4>
-            <p>I switched to this (dining) table top from a Bekant 120x60 and DIY drilled to the frame. I love desks
-              with larger depth, however it's not the best, but I have decent space for all of my stuff.
+            <h4><a href="https://www.ikea.com/gb/en/p/oevraryd-table-top-bamboo-90305723/">IKEA Övraryd table top, bamboo, 150x78</a></h4>
+            <p>It's originally a dining table top from IKEA, but I drilled it to the Autonomous frame. I love desks
+              with more depth, and having a decent space for all of my stuff.
             </p>
           </div>
           <div>
-            <h4><a href="https://amzn.to/3pBnZpo">TaoTronics LED Desk Lamp, 14W with USB Charging Port</a></h4>
-            <p>I was looking for a stylish LED table lamp when I found this fine product of TaoTronics. Not perfect, but
-              it does the job.
-            </p>
+            <h4><a href="https://www.mi.com/uk/product/mi-computer-monitor-light-bar/">Xiaomi Monitor Light bar</a></h4>
+            <p>People had been talking about it in the office, and I finally gave it a go after a few months. Immediately dropped
+              my desk lamp and saved more space. It's hard to find the best position on the top of the monitor when
+              you also have a webcam, but the remote control is very cool.</p>
           </div>
         </div>
         <div class="md:w-1/2 md:pl-6">
           <h2>Software</h2>
           <div>
             <h4><a href="https://www.jetbrains.com/phpstorm/">PHPStorm</a></h4>
-            <p>The best IDE for PHP and web development: it knows everything from Git through Docker to databases. I'm
-              using it with light theme.
+            <p>The best IDE for PHP and web development: it has a client for everything from Git conflicts through Docker and databases. I'm
+              intentionally <a href="https://stitcher.io/blog/why-light-themes-are-better-according-to-science">using it with a light theme</a>.
             </p>
           </div>
-          <h4><a href="https://www.sublimetext.com/">Sublime Text</a></h4>
-          <p>When I need to edit something quickly or there's a larger database dump or CSV file to open I just launch
-            Sublime instead.
-          </p>
         </div>
       </div>
       <h2>Music production</h2>
@@ -88,7 +87,9 @@
         <div class="md:w-1/2 md:pr-6">
           <div>
             <h4><a href="https://amzn.to/3lJxwbR">KRK Rokit 5 G4 White Noise</a></h4>
-            <p>They're brilliant active monitor speakers with good enough bass for a small room.</p>
+            <p>They're brilliant active monitor speakers with good enough bass for a small room. After two years I
+              bought a standing desk clamp for both, now they're using less space on my desk and positioned to the
+              same height where my ears are.</p>
           </div>
           <div>
             <h4><a href="https://www.ableton.com/en/live/">Ableton Live</a></h4>
@@ -124,7 +125,8 @@
           <div>
             <h4><a href="https://amzn.to/2VHaFmp">Audio-Technica ATH-M50X</a></h4>
             <p>Wireless isn't good enough for mixing when you need instant feedback. However be careful with the
-              volume: it's really crisp and very loud!
+              volume: it's really crisp and very loud! The only downside is that I have to replace the ear pads every
+              two or three years.
             </p>
           </div>
         </div>
