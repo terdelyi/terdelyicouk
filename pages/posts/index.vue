7cce38f4aa3624876f3f8bdbe141589845978a47
Tamas Erdelyi <terdelyi@users.noreply.github.com>
2023-09-01 08:48:03 +0100

    New post: User to engineer (#18)

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,4 +1,7 @@
 <script setup lang="ts">
+import type { QueryBuilderParams } from '@nuxt/content/dist/runtime/types'
+const query: QueryBuilderParams = { path: '/posts', sort: [{ date: -1 }] }
+
 useHead({
   title: 'Posts',
 })
@@ -8,7 +11,7 @@ useSeoMeta({
 </script>
 
 <template>
-  <ContentList path="/posts">
+  <ContentList :query="query">
     <template #default="{ list }">
       <article v-for="post in list" :key="post._path" class="mb-10">
         <h3 class="font-header text-xl font-bold mb-2 leading-tight tracking-tight underline"><NuxtLink :to="post._path" class="text-pink hover:text-blue-600 dark:hover:text-grey-400 hover:underline">{{ post.title }}</NuxtLink></h3>
