44494407e7a407a89f69de945586dc9b2e1e2ed8
Tamas Erdelyi <terdelyi@users.noreply.github.com>
2024-07-09 23:59:06 +0100

    Update Phanstatic (#27)
    * Update Phanstatic and configuration

--- content/config.php
+++ content/config.php
@@ -2,10 +2,19 @@
 
 use Terdelyi\Phanstatic\Config\ConfigBuilder;
 
-$baseUrl = getenv('ENV') === 'production' ? getenv('URL') : getenv('DEPLOY_URL') ?? 'http://localhost:8000';
+if (getenv('ENV') === 'production') {
+    $baseUrl = getenv('URL');
+} else {
+    $baseUrl = getenv('DEPLOY_URL') ? getenv('DEPLOY_URL') : 'http://localhost:8000';
+}
 
-return (new ConfigBuilder)
+return ConfigBuilder::make()
     ->setBaseUrl($baseUrl)
     ->setTitle('Tamas Erdelyi')
-    ->addCollection('posts', 'Posts', 'posts', 10)
+    ->addCollection(
+        'posts',
+        'Posts',
+        'posts',
+        10
+    )
     ->build();
