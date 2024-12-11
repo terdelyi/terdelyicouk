88e8df0b7fcae234240a8925b8003823583ac0ff
Tamas Erdelyi <623128+terdelyi@users.noreply.github.com>
2024-12-11 17:19:33 +0000

    View bugfixes and add Talks to menu (#36)

--- content/pages/_partials/header.php
+++ content/pages/_partials/header.php
@@ -27,18 +27,19 @@ $image = $page->image ?? 'images/generic.png';
     </script>
 </head>
 <body class="bg-white-bg dark:bg-grey-700 font-sans text-grey-body dark:text-grey-500 antialiased">
-    <div class="flex flex-col h-screen justify-between">
+    <div class="flex flex-col min-h-screen">
         <header class="bg-blue-400 relative dark:bg-grey-800 text-grey-200 dark:text-grey-400">
-            <div class="w-full md:max-w-screen-lg flex items-center justify-between mx-auto">
-                <div class="py-4 pl-4 lg:pl-0">
-                        <h1 class="sm:text-xl">
-                            <a href="/" class="hover:underline font-bold tracking-tight">Tamas Erdelyi</a>
-                        </h1>
+            <div class="w-full md:max-w-screen-lg flex items-center justify-between mx-auto h-14">
+                <div class="pl-4 lg:pl-0">
+                    <h1 class="sm:text-xl">
+                        <a href="/" class="hover:underline font-bold tracking-tight">Tamas Erdelyi</a>
+                    </h1>
                 </div>
-                <nav class="hidden sm:block pr-12 lg:pr-0">
+                <nav class="hidden sm:block pr-12 sm:pr-12 md:pr-20 xl:pr-0">
                     <ul class="px-4 md:px-0 text-sm md:text-base font-normal tracking-tight inline-flex">
                         <li><a href="/about/" class="px-4 sm:px-3 hover:underline<?php if ($page->is('/about/')) :?> font-bold<?php endif; ?>">About</a></li>
                         <li><a href="/posts/" class="px-4 sm:px-3 hover:underline<?php if ($page->is('/posts/') || $page->is('posts')) :?> font-bold<?php endif; ?>">Posts</a></li>
+                        <li><a href="/talks/" class="px-4 sm:px-3 hover:underline<?php if ($page->is('/talks/')) :?> font-bold<?php endif; ?>">Talks</a></li>
                         <li><a href="/uses/" class="px-4 sm:px-3 hover:underline<?php if ($page->is('/uses/')) :?> font-bold<?php endif; ?>">Uses</a></li>
                         <li><a href="/contact/" class="px-4 md:pr-0 sm:px-4 hover:underline<?php if ($page->is('/contact/')) :?> font-bold<?php endif; ?>">Contact</a></li>
                     </ul>
@@ -56,15 +57,16 @@ $image = $page->image ?? 'images/generic.png';
                     </button>
                 </div>
             </div>
-            <div id="mobile-menu" class="max-w-screen-md sm:hidden px-4 md:px-0 py-2 mx-auto bg-blue-700 dark:bg-grey-750 hidden">
+            <div id="mobile-menu" class="max-w-screen-md sm:hidden px-6 md:px-0 pb-4 mx-auto bg-gradient-to-b from-blue-400 dark:from-grey-800 to-blue-700 dark:to-grey-750 hidden">
                 <ul>
                     <li class="pb-1"><a href="/about/" class="hover:underline<?php if ($page->is('/about/')) :?> font-bold text-grey-100<?php endif; ?>">About</a></li>
                     <li class="pb-1"><a href="/posts/" class="hover:underline<?php if ($page->is('/posts/')) :?> font-bold text-grey-100<?php endif; ?>">Posts</a></li>
+                    <li class="pb-1"><a href="/talks/" class="hover:underline<?php if ($page->is('/uses/')) :?> font-bold text-grey-100<?php endif; ?>">Talks</a></li>
                     <li class="pb-1"><a href="/uses/" class="hover:underline<?php if ($page->is('/uses/')) :?> font-bold text-grey-100<?php endif; ?>">Uses</a></li>
                     <li class="pb-1"><a href="/contact/" class="hover:underline<?php if ($page->is('/contact/')) :?> font-bold text-grey-100<?php endif; ?>">Contact</a></li>
                 </ul>
             </div>
-            <div class="absolute top-[22px] sm:top-5 right-14 sm:right-6 hover:text-white">
+            <div class="absolute top-[18px] sm:top-4 right-14 sm:right-6 hover:text-white">
                 <a href="#" id="toggle-switch" title="Toggle theme between light and dark" aria-label="Toggle theme between light and dark">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 23 23" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" id="toggle-switch-off" class="w-5 feather feather-sun"><circle cx="12" cy="12" r="5"></circle><line x1="12" y1="1" x2="12" y2="3"></line><line x1="12" y1="21" x2="12" y2="23"></line><line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line><line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line><line x1="1" y1="12" x2="3" y2="12"></line><line x1="21" y1="12" x2="23" y2="12"></line><line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line><line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line></svg>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 23 23" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" id="toggle-switch-on" class="hidden w-5 feather feather-moon"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
@@ -72,8 +74,5 @@ $image = $page->image ?? 'images/generic.png';
             </div>
         </header>
 
-        <?php if ($page->permalink === '/') : ?>
-        <main class="flex h-screen mb-auto px-4 sm:px-0 container-md">
-        <?php else : ?>
-        <main class="mb-auto px-4 sm:px-0 container-md pt-5 sm:pt-10 pb-4 sm:pb-6">
-        <?php endif; ?>
+
+        <main class="px-4 lg:px-0 container-md <?php if ($page->permalink === '/') : ?>flex-grow flex justify-center items-center<?php else : ?>pt-5 sm:pt-10 pb-4 sm:pb-6 flex-grow<?php endif; ?>">
